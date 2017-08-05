d223d58b485ddeb441d8f4aca88cc09c954f1736
JSmith <jaysmit158@gmail.com>
2017-08-04 20:22:17 -0400

    Added grades of other students

--- reportcard.js
+++ reportcard.js
@@ -1,7 +1,9 @@
 'use strict';
 
+//This
+
 class Student {
-  constructor({ name, age, interestLevel = 5 } = {}) {
+  constructor({ name, age, interestLevel} = {}) {
     this.name = name;
     this.age = age;
     this.interestLevel = interestLevel;
@@ -9,10 +11,32 @@ class Student {
   }
 }
 
-let joey = new Student({ name: 'Joey', age: 25 });
-let sarah = new Student({ name: 'Sarah', age: 22 });
+let joey = new Student({ name: 'Joey', age: 14 });
+let sarah = new Student({ name: 'Sarah', age: 14 });
+let alex = new Student({ name: 'Alex', age: 15 });
+let blake = new Student({ name: 'Blake', age: 13 });
+
+
+joey.grades.set('History', 'C');
+joey.grades.set('Math', 'B');
+joey.grades.set('Science', 'A');
+joey.grades.set('English', 'C');
+joey.grades.set('Spanish', 'A');
 
 sarah.grades.set('History', 'B');
 sarah.grades.set('Math', 'A');
+sarah.grades.set('Science', 'A');
+sarah.grades.set('English', 'B');
 
-console.log(Array.from(sarah.grades));
+alex.grades.set('History', 'F');
+alex.grades.set('Math', 'C');
+alex.grades.set('Science', 'C');
+alex.grades.set('English', 'D');
+
+blake.grades.set('History', 'A');
+blake.grades.set('Math', 'C');
+blake.grades.set('Science', 'C');
+blake.grades.set('English', 'A');
+
+
+console.log(Array.from(sarah.grades),);
